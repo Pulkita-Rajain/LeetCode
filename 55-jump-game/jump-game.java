39a47267f37cd22c527814fc828b7f38603b7d1d
Pulkita Rajain <128276984+Pulkita-Rajain@users.noreply.github.com>
2026-05-30 17:35:22 +0530

    Time: 1 ms (99.98%) | Memory: 47.9 MB (41.36%) - LeetSync

--- 55-jump-game/jump-game.java
+++ 55-jump-game/jump-game.java
@@ -1,18 +1,13 @@
 class Solution {
     public boolean canJump(int[] nums) {
-        // Take curr variable to keep the current maximum jump...
-        int curr = 0;
-        // Traverse all the elements through loop...
-        for (int i = 0; i < nums.length; i++) {
-            // If the current index 'i' is less than current maximum jump 'curr'...
-            // It means there is no way to jump to current index...
-            // so we should return false...
-            if (i > curr) {
-                return false;
+        int goal = nums.length - 1;
+
+        for (int i = nums.length - 2; i >= 0; i--) {
+            if (i + nums[i] >= goal) {
+                goal = i;
             }
-            // Update the current maximum jump...
-            curr = Math.max(curr, i + nums[i]);     // It’s possible to reach the end of the array...
         }
-        return true;
+
+        return goal == 0;        
     }
 }
